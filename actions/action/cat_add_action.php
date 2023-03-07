13a04ec37c9109aa87023f2e89bace31667d87b8
Rikken, Bas - Student Rijn IJssel <00298147@rijnijssel.nl>
2023-03-07 16:42:25 +0100

    Version 1.4 - "Can now add categories, no display yet though"

--- actions/action/cat_add_action.php
+++ actions/action/cat_add_action.php
@@ -1,1 +1,20 @@
 <?php
+session_start();
+require '../../../private/conn_Webshop.php';
+
+$newcat = $_POST['category_name'];
+
+$sth = $dbh->prepare("
+    INSERT INTO 
+    categories 
+        (category_name) 
+    VALUE 
+        (:newcat)",
+    array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+$sth->execute([
+    ':newcat' => $newcat
+]);
+$_SESSION['CatAddSuccess'] = 'true';
+header('Location: ../../Webshop/index.php?page=category_add');
+?>
