23ddfb84897d93e5db9d2626d37b76c95750be57
Rikken, Bas - Student Rijn IJssel <00298147@rijnijssel.nl>
2023-04-12 16:26:55 +0200

    Version 3.8.8 - Images can now be edited through file selections

--- actions/action/product_edit_action.php
+++ actions/action/product_edit_action.php
@@ -7,37 +7,16 @@ $product_name = $_POST['Product_name'];
 $product_EAN = $_POST['Product_ean'];
 $product_amount = $_POST['Product_amount'];
 $product_price = $_POST['Product_price'];
-$product_image = $_POST['Product_img'];
+if (isset($_SESSION['img_edit'])){
+    $product_image = base64_encode(file_get_contents($_FILES['img']['tmp_name']));
+}
 $product_category = $_POST['category'];
 
 if ($product_name != '' &&
     $product_EAN != '' &&
     $product_amount != '' &&
     $product_price != '') {
-    if ($product_image == '') {
-        try {
-            $stmt = $dbh->prepare("UPDATE products
-SET product_name = :newname,
-    product_EAN = :newEAN, 
-    product_amount = :newamount, 
-    product_price = :newprice,
-    FKcategory_id = :newcategory
-WHERE product_id = :proid", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-
-            $stmt->execute([
-                ':proid' => $productid,
-                ':newname' => $product_name,
-                ':newEAN' => $product_EAN,
-                ':newamount' => $product_amount,
-                ':newprice' => $product_price,
-                ':newcategory' => $product_category
-            ]);
-            $_SESSION['product_edit_success'] = 'true';
-            header('Location:  ../../index.php?page=product_edit');
-        } catch (PDOException $exception) {
-            $exception->getMessage();
-        }
-    } else {
+    if (isset($_SESSION['img_edit'])) {
         try {
             $stmt = $dbh->prepare("UPDATE products
 SET product_name = :newname,
@@ -57,6 +36,31 @@ WHERE product_id = :proid", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
                 ':newimg' => $product_image,
                 ':newcategory' => $product_category
 
+            ]);
+            $_SESSION['product_edit_success'] = 'true';
+            unset($_SESSION['img_edit']);
+            header('Location:  ../../index.php?page=product_edit');
+        } catch (PDOException $exception) {
+            $exception->getMessage();
+        }
+
+    } else {
+        try {
+            $stmt = $dbh->prepare("UPDATE products
+SET product_name = :newname,
+    product_EAN = :newEAN, 
+    product_amount = :newamount, 
+    product_price = :newprice,
+    FKcategory_id = :newcategory
+WHERE product_id = :proid", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+            $stmt->execute([
+                ':proid' => $productid,
+                ':newname' => $product_name,
+                ':newEAN' => $product_EAN,
+                ':newamount' => $product_amount,
+                ':newprice' => $product_price,
+                ':newcategory' => $product_category
             ]);
             $_SESSION['product_edit_success'] = 'true';
             header('Location:  ../../index.php?page=product_edit');
--- includes/contents/product_edit_page.php
+++ includes/contents/product_edit_page.php
@@ -5,49 +5,71 @@ $productEAN = $_GET['product_ean'];
 $productamount = $_GET['product_amount'];
 $productprice = $_GET['product_price'];
 
+if (isset($_GET['img'])) {
+    $_SESSION['img_edit'] = 'true';
+}
+
 if (isset($_SESSION['product_edit_fail'])) {
     echo '<script type="text/javascript">
        window.onload = function () { alert("Not all required fields are filled in."); } 
 </script>';
     unset($_SESSION['product_edit_fail']);
 }
-
-echo '</div>
+?>
+</div>
 <div class="editinput">
-    <form action="actions/action/product_edit_action.php?product_id=' . $productid . '" method="post" id="Edit">
+    <form action="actions/action/product_edit_action.php?product_id=<?=$productid?>" method="post"
+          enctype="multipart/form-data" id="Edit">
         <label for="Product_name">
             <b>Name</b>
         </label>
-        <input type="text" placeholder="Enter product name" name="Product_name" value="' . $productname . '" required>
-        
+        <input type="text" placeholder="Enter product name" name="Product_name" value="<?= $productname ?>" required>
+
         <label for="Product_ean">
             <b>EAN code</b>
         </label>
-        <input type="text" placeholder="Enter product name" name="Product_ean" value="' . $productEAN . '" required>
-        
+        <input type="text" placeholder="Enter product name" name="Product_ean" value="<?= $productEAN ?>" required>
+
         <label for="Product_amount">
             <b>Amount</b>
         </label>
-        <input type="text" placeholder="Enter amount" name="Product_amount" value="' . $productamount . '" required min="1">
-        
+        <input type="text" placeholder="Enter amount" name="Product_amount" value="<?= $productamount ?>" required
+               min="1">
+
         <label for="Product_price">
             <b>Price</b>
         </label>
-        <input type="text" placeholder="Enter product price" name="Product_price" value="' . $productprice . '" required min="1">
-        
-        <label for="Product_img">
-            <b>Image</b>
-        </label>
-        <input type="text" placeholder="Enter product image url" name="Product_img">
-        
-        ';
-include 'actions/sa/product_cat_sa.php';
-echo '</form>';
-?>
+        <input type="text" placeholder="Enter product price" name="Product_price" value="<?= $productprice ?>" required
+               min="1">
+
+        <?php
+        if (!isset($_SESSION['img_edit'])) {
+            ?>
+            <p><button class=""
+                    onclick="location.href='../../Webshop/index.php?page=product_edit_page&img=1&product_id=<?= $productid ?>&product_name=<?= $productname ?>&product_ean=<?= $productEAN ?>&product_amount=<?= $productamount ?>&product_price=<?= $productprice ?>'"
+                    type="button">Edit Image
+            </button></p>
+            <?php
+        } else {
+            ?>
+            <label for="img">
+                <b>Image</b>
+            </label>
+            <input type="file" accept="image/*" name="img">
+            <?php
+        }
+        ?>
+        <?php
+        include 'actions/sa/product_cat_sa.php';
+        ?>
+    </form>
+
     <button onclick="return confirm('Are you sure you want to make this edit?')" class="loginsubmit" type="submit"
             form="Edit">
-<?php
-echo 'Edit   
+        Edit
     </button>
 
-</div>';
+</div>
+
+
+
